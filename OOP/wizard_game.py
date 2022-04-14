0f452594bd444a1ac53d216681be2972fd2a01cc
Gokul Kamat <gokulgkamat@gmail.com>
2022-04-14 18:31:28 +0530

    Learning about class object attributes, methods and __init__

--- OOP/wizard_game.py
+++ OOP/wizard_game.py
@@ -1,11 +1,17 @@
 class PlayerCharacter:
-    def __init__(self, name, age):
-        self.name = name # attributes
-        self.age = age
+    membership = True # Class Object Attribute
+    def __init__(self, name='anonymous', age=0):
+        # We can do this to refer to `membership` => if PlayerCharacter.membership:
+        # or do this => if self.membership:
+        if age > 18: # the attributes are only initialised if age > 18
+            self.name = name # attributes
+            self.age = age
 
-    def run(self):
-        print('run')
-        return 'done'
+    def shout(self):
+        print(f'my name is {self.name}')
+
+    def run(self, hello):
+        print(hello)
 
 player1 = PlayerCharacter('Cindy', 44)
 player2 = PlayerCharacter('Tom', 21)
@@ -15,4 +21,6 @@ print(player1.name)
 print(player1.age)
 print(player2.name)
 print(player2.age)
-print(player1.run())
+print(player1.shout())
+print(player2.membership)
+print(player1.run("Hello!"))
