23f0f879ba36dcef6f98d68e16b4a73388f2d4b8
Gokul Kamat <gokulgkamat@gmail.com>
2022-04-14 19:14:16 +0530

    Learning about the decorators @classmethod and @staticmethod

--- OOP/wizard_game.py
+++ OOP/wizard_game.py
@@ -10,17 +10,26 @@ class PlayerCharacter:
     def shout(self):
         print(f'my name is {self.name}')
 
-    def run(self, hello):
-        print(hello)
+    @classmethod
+    def adding_things(cls, num1, num2): # `cls` is the class that is taken as a parameter
+        return cls("Orion", num1 + num2)
+
+    @staticmethod
+    def adding_things2(num1, num2): # `cls` is not taken as a parameter here
+        return num1 + num2
 
 player1 = PlayerCharacter('Cindy', 44)
 player2 = PlayerCharacter('Tom', 21)
 player2.attack = 50  # adds an attack attribute to player2
 
-print(player1.name)
-print(player1.age)
-print(player2.name)
-print(player2.age)
-print(player1.shout())
-print(player2.membership)
-print(player1.run("Hello!"))
+# print(PlayerCharacter.adding_things(2, 3)) => this is done to access methods with the @classmethod decorator
+player3 = PlayerCharacter.adding_things(10, 9)
+print(player3.age)
+
+# print(player1.name)
+# print(player1.age)
+# print(player2.name)
+# print(player2.age)
+# print(player1.shout())
+# print(player2.membership)
+# print(player1.run("Hello!"))
